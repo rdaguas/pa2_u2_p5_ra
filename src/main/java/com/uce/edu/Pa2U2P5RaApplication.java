c684c6c4fada1a3519028c9baf6fcfdcf1ed0397
rdaguas <aguas.rdv2001@gmail.com>
2024-01-05 16:19:31 -0500

    Tarea7

--- src/main/java/com/uce/edu/Pa2U2P5RaApplication.java
+++ src/main/java/com/uce/edu/Pa2U2P5RaApplication.java
@@ -1,23 +1,26 @@
 package com.uce.edu;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.repository.modelo.Habitacion;
-import com.uce.edu.repository.modelo.Hotel;
-import com.uce.edu.service.IHotelService;
+import com.uce.edu.repository.modelo.Ciudadano;
+import com.uce.edu.repository.modelo.Empleado;
+import com.uce.edu.service.ICiudadanoService;
+import com.uce.edu.service.IEmpleadoService;
 
 @SpringBootApplication
 public class Pa2U2P5RaApplication implements CommandLineRunner {
 
 	@Autowired
-	private IHotelService iHotelService;
+	private ICiudadanoService iCiudadanoService;
 
+	@Autowired
+	private IEmpleadoService iEmpleadoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U2P5RaApplication.class, args);
@@ -27,26 +30,46 @@ public class Pa2U2P5RaApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
-		Hotel hote = new Hotel();
-		hote.setNombre("Marriot");
-		hote.setDireccion("Av.Colon");
-		//hote.setHabitaciones(null);
-		
-		Habitacion h1 = new Habitacion();
-		h1.setClase("Economica");
-		h1.setNumero("A1");
-		h1.setHotel(hote);
-		
-		Habitacion h2 = new Habitacion();
-		h2.setClase("VIP");
-		h2.setNumero("A2");
-		h2.setHotel(hote);
-		
-		List<Habitacion> habitaciones = new ArrayList<>();
-		habitaciones.add(h1);
-		habitaciones.add(h2);
-		hote.setHabitaciones(habitaciones);
-		this.iHotelService.guardar(hote);
+		// Empleado
+		Empleado empl = new Empleado();
+		empl.setSalario(new BigDecimal(600));
+		empl.setFechaIngreso(LocalDateTime.now());
+		//this.iEmpleadoService.guardar(empl);
+
+		// Ciudadano
+		// Insertar Ciudadano clase mater o Padre
+		Ciudadano ciud = new Ciudadano();
+		ciud.setNombre("Martha");
+		ciud.setApellido("Valencia");
+		ciud.setEmpleado(empl);
+		empl.setCiudadano(ciud);
+		this.iCiudadanoService.guardar(ciud);
+
+		// Actualizar
+		 Ciudadano ciud2 = this.iCiudadanoService.buscar(1);
+		 ciud2.setNombre("Daniel");
+		 this.iCiudadanoService.actualizar(ciud2);
+
+		// Buscar
+		 Ciudadano ciud1 = this.iCiudadanoService.buscar(6);
+		 System.out.println(ciud1);
+
+		// eliminar un Ciudadano
+		 this.iCiudadanoService.eliminar(7);
+
+		// Buscar
+		 Empleado emple1 = this.iEmpleadoService.buscar(1);
+		 System.out.println(emple1);
+
+		// actualizar
+		 Empleado empl2 = this.iEmpleadoService.buscar(2);
+		 empl2.setSalario(new BigDecimal(780));
+		 this.iEmpleadoService.actualizar(empl2);
+
+		// Eliminar
+
+		 this.iEmpleadoService.eliminar(4);
+
 	}
 
 }
